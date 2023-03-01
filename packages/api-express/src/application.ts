0f403ccdc2e618374f6749024d3ebee310cee899
Nándor Dudás <nandor.dudas@gmail.com>
2023-03-01 17:24:22 +0000

    feat: add logger (APP-18)

--- packages/api-express/src/application.ts
+++ packages/api-express/src/application.ts
@@ -1,8 +1,15 @@
 import express, { json, urlencoded } from 'express'
+import morgan, { type StreamOptions } from 'morgan'
 
 import Database from '~/Database/Database'
 import errorMiddleware from '~/Middleware/error'
 import TodoRouter from '~/Routers/TodoRouter'
+
+import { logger } from './logger'
+
+const stream: StreamOptions = {
+  write: message => logger.http(message),
+}
 
 export class Application {
   public static main() {
@@ -11,14 +18,17 @@ export class Application {
     if (!db.isConnected())
       throw new Error('Database connection failed.')
 
-    // eslint-disable-next-line no-console
-    console.log('⚡️[database]: Database is connected successfully.')
+    logger.info('⚡️[database]: Database is connected successfully.')
 
     const app = express()
     const { router: todoRouter } = new TodoRouter()
 
     return app
-      .use(json(), urlencoded({ extended: true }))
+      .use(
+        json(),
+        urlencoded({ extended: true }),
+        morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms', { stream }),
+      )
       .use('/', todoRouter)
       .use(errorMiddleware)
   }
