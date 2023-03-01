b19ffb935bfa5a25ee754f5c544e84e3313dbe0e
Nándor Dudás <nandor.dudas@gmail.com>
2023-03-01 11:41:08 +0000

    chore(deps): update

--- config/commitlint/src/index.cjs
+++ config/commitlint/src/index.cjs
@@ -1,22 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { RuleConfigSeverity } = require('@commitlint/types')
 
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-  ],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'references-empty': [
-      RuleConfigSeverity.Error,
-      'never',
-    ],
+    'references-empty': [RuleConfigSeverity.Error, 'never'],
   },
   parserPreset: {
     parserOpts: {
-      issuePrefixes: [
-        'APP-',
-      ],
+      issuePrefixes: ['APP-'],
     },
   },
 }
